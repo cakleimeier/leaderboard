1420bb5a81425c54039cc591e6fbb1085bf30001
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2017-11-09 16:31:12 -0500

    Create functionality, styles

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -10,110 +10,87 @@ export class ListContainer extends Component {
     super(props);
     this.state= {
       time: 'recent',
-      list: []
+      recentList: [],
+      totalList: []
     };
 
     this.handleClick = this.handleClick.bind(this);
-    this.getList = this.getList.bind(this);
   }
-
-  // Thoughts
-    // You want handleClick to set the state and run the getList function
-    // You want to pass the result of the getList function as a prop
-    // to the List component
-    // the List component will display that result
-    // Meaning: give the <List /> the value when you call it
-    // ex: <List campers={this.props.list} />
-
-    // I think the Table component should actually be the parent?
-    // The Table component runs the handleClick function onClick
 
   handleClick(e) {
     this.setState({
       'time': e.target.id
-    }, this.getList);
+    });
   }
 
   getList() {
-    var list;
-    if (this.state.time === "recent") {
-      // Make AJAX call
-      // https://fcctop100.herokuapp.com/api/fccusers/top/recent
-      fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-        .then(function(response) {
-          return response.json()
-        }).then(json => {
-          console.log(json);
-          list = json.map(obj => obj.data);
-          this.setState({list}, console.log(this.state.list));
-        })
-        .catch(function(ex) {
-          console.log('parsing failed', ex)
-      });
+    var apiRequest1 = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(function(response){ 
+      return response.json();
+    });
+    var apiRequest2 = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(function(response){
+      return response.json();
+    });
+    var recentList=[];
+    var totalList=[];
 
-    } else {
-      // Make AJAX call
-      // https://fcctop100.herokuapp.com/api/fccusers/top/alltime
-      fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-        .then(function(response) {
-          return response.json()
-        }).then(json => {
-          console.log(json);
-          list = json.map(obj => obj.data);
-          this.setState({list}, console.log(this.state.list));
-        })
-        .catch(function(ex) {
-          console.log('parsing failed', ex)
-      });
-    }
+    Promise.all([apiRequest1,apiRequest2]).then(values=>{
+        //combinedData[0] = values[0];
+        //combinedData[1] = values[1];
+        for (const prop in values[0]) {
+          recentList.push([]);
+          for(const innerProp in values[0][prop]) {
+            recentList[prop].push(values[0][prop][innerProp]);
+          }
+        }
+        for (const prop in values[1]) {
+          totalList.push([]);
+          for(const innerProp in values[1][prop]) {
+            totalList[prop].push(values[1][prop][innerProp]);
+          }
+        }
+        this.setState({
+          recentList: recentList,
+          totalList: totalList
+        });
+        return recentList, totalList;
+    });
+  }
+
+  componentDidMount() {
+    this.getList();
   }
 
   render() {
-    return <Table list={this.state.list} onClick={this.handleClick} />
+    return <Table list={this.state.time === "recent"? this.state.recentList : this.state.totalList} onClick={this.handleClick} time={this.state.time} />
   }
 }
 
 class List extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return (
-        <tbody>
-          <tr>
-            <td>Test</td>
-            <td>{this.props.list}</td>
-          </tr>
-        </tbody>
+    var list= this.props.list;
+    console.log(list);
 
-        /*
-          {this.props.list.map((user,i) =>
-            <tr key={user}><td>{user}</td></tr>
-          )}
-
-          <tr>
-            <td>Postion</td>
+    return(
+      <tbody>
+        {list.map((user,i) =>
+          <tr key={i}>
+            <td>{i+1}</td>
+            <td>
+              <a href={"https://www.freecodecamp.com/"+ user[0]}>
+                <img src={user[1]} style={{width: '50px'}} />
+                {user[0]}
+              </a>
+            </td>
+            <td className={(this.props.time=="recent" ? 'active' : '')}>{this.props.time==="recent"?user[2]:user[3]}</td>
+            <td className={(this.props.time=="total" ? 'active' : '')}>{this.props.time==="recent"?user[3]:user[2]}</td>
           </tr>
-          <tr>
-            <td>Camper</td>
-          </tr>
-          <tr>
-            <td>Recent Points</td>
-          </tr>
-          <tr>
-            <td>Total</td>
-          </tr>
-      */
+        )}
+      </tbody>
     );
   }
 }
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <table>
@@ -121,19 +98,13 @@ class Table extends Component {
         <thead>
           <tr>
             <th>Postion</th>
-          </tr>
-          <tr>
             <th>Camper</th>
-          </tr>
-          <tr>
-            <th style={{cursor:'pointer', background: '#cdecde'}} onClick={this.props.onClick} id="recent">Recent Points</th>
-          </tr>
-          <tr>
-            <th style={{cursor:'pointer', background: '#cdecde'}} onClick={this.props.onClick} id="total">Total Points</th>
+            <th className={"sort" + (this.props.time=="recent" ? ' active' : '')} onClick={this.props.onClick} id="recent">Recent Points {this.props.time=="recent" ? String.fromCharCode("9662") : ''}</th>
+            <th className={"sort" + (this.props.time=="total" ? ' active' : '')} onClick={this.props.onClick} id="total">Total Points {this.props.time=="total" ? String.fromCharCode("9662") : ''}</th>
           </tr>
         </thead>
 
-        <List list={this.props.list} />
+        <List list={this.props.list} time={this.props.time} />
 
       </table>
     );
